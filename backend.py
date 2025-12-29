be4a434fddc8af661bfafe4dacd913f3e44aaeb0
munesh kumar <mkumar21@lululemon.com>
2025-12-29 12:26:48 +0530

    update port number for access

--- backend.py
+++ backend.py
@@ -62,7 +62,7 @@ def chat_endpoint(request: RequestState):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=9999)
+    uvicorn.run(app, host="0.0.0.0", port=9999)
 
     # Swagger UI will give UI for test the Fast API request.
     # strat the server , just run backend.py file.
